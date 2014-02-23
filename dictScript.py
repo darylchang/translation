de80e0173968b40e46ad101fa4faf8f4973c717e
Daryl Chang <darylc123@gmail.com>
2014-02-22 16:32:21 -0800

    Trying to pull

--- dictScript.py
+++ dictScript.py
@@ -1,1 +1,12 @@
-import urllib
+import urllib2
+from bs4 import BeautifulSoup
+
+url = 'https://www.translate.google.com/#es/en/' + 'queda'
+headers = {'User-Agent': 'Magic Browser'}
+req = urllib2.Request(url, None, headers=headers)
+res = urllib2.urlopen(req)
+html = res.read()
+soup = BeautifulSoup(html)
+
+print soup.prettify()
+words = soup.find_all('div', {'class' : 'gt-baf-word-clickable'})
