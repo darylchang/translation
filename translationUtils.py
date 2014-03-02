de48af36bb6b0a66c82af0fb8e349573dc63ac56
Keith Wyngarden <kwyngard@stanford.edu>
2014-03-01 23:32:51 -0800

    Less aggressive stop word removal

--- translationUtils.py
+++ translationUtils.py
@@ -1,6 +1,17 @@
 import copy
 
-def iterBools(lists, remaining):
+# Return whether or not the previous token in the sentence was removed
+# as a stop word. This is true if:
+#       1. Last element of list is True
+#       2. Previous index of removable token is directly before this one
+def prevTokenRemoved(removePositions, index, sublist):
+    if not sublist[-1]:
+        return False
+    if index == 0 or removePositions[index-1] < (removePositions[index]-1):
+        return False
+    return False
+
+def iterBools(lists, removePositions, index, remaining):
     if not lists and remaining > 0:
         lists = [[True], [False]]
         remaining -= 1
@@ -8,9 +19,12 @@ def iterBools(lists, remaining):
         return lists
     newLists = []
     for sublist in lists:
-        trueList = copy.deepcopy(sublist)
         falseList = copy.deepcopy(sublist)
-        trueList.append(True)
         falseList.append(False)
-        newLists.extend([trueList, falseList])
-    return iterBools(newLists, remaining - 1)
+        newLists.append(falseList)
+
+        if not prevTokenRemoved(removePositions, index, sublist):
+            trueList = copy.deepcopy(sublist)
+            trueList.append(True)
+            newLists.append(trueList)
+    return iterBools(newLists, removePositions, index + 1, remaining - 1)
